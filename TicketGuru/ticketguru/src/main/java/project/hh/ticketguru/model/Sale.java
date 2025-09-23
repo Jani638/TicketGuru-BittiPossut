de7a745839af3256656dbbf67dd252791bffcfde
Konsta Lyytikäinen <bhq669@myy.haaga-helia.fi>
2025-09-23 20:57:08 +0300

    ylimääränen koodi pois

--- TicketGuru/ticketguru/src/main/java/project/hh/ticketguru/model/Sale.java
+++ TicketGuru/ticketguru/src/main/java/project/hh/ticketguru/model/Sale.java
@@ -12,16 +12,11 @@ import jakarta.persistence.OneToOne;
 public class Sale {
 
     @Id
-<<<<<<< HEAD
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-=======
-<<<<<<< HEAD
     @GeneratedValue(strategy = GenerationType.AUTO)
 
-=======
     @GeneratedValue(strategy = GenerationType.IDENTITY)
->>>>>>> ba3df76276a725581f3b80ea90182795a1d8f1b7
->>>>>>> fe0f99622c41a0b05de4b09d9ebd5aa96a030903
+
     private Long id;
     
     private Long ticketId;
@@ -39,12 +34,9 @@ public class Sale {
 
     private LocalDateTime saleDate;
 
-<<<<<<< HEAD
     public Sale() {
     }
-=======
     public Sale(){}
->>>>>>> ba3df76276a725581f3b80ea90182795a1d8f1b7
 
     public Sale(Long id, Long ticketId, Long customerId, Long sellerId, LocalDateTime saleDate) {
         this.id = id;
