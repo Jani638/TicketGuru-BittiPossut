c1a63a1e56139e12f8a12bf84c25f959d030cb72
MarianneTaipale <marianne.taipale@gmail.com>
2025-10-27 16:48:36 +0200

    application.properties muutoksia

--- TicketGuru/ticketguru/src/main/java/project/hh/ticketguru/model/User.java
+++ TicketGuru/ticketguru/src/main/java/project/hh/ticketguru/model/User.java
@@ -1,7 +1,5 @@
 package project.hh.ticketguru.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
